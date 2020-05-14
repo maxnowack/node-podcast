ff823aa39c1400032887f68678dfe2460d503529
Teun Duynstee <github@duynstee.com>
2020-05-14 12:47:58 +0200

    Fix: items passed through contructor appeared as duplicates (#34)

--- test/index.js
+++ test/index.js
@@ -62,6 +62,53 @@ test('podcast', (t) => {
 
   t.is(feed.buildXml({ indent: '  ' }), expectedOutput.podcast.trim());
 })
+test('podcast using contructor with items', (t) => {
+  const feedInfo = {
+    title: 'title',
+    description: 'description',
+    feed_url: 'http://example.com/rss.xml',
+    site_url: 'http://example.com',
+    author: 'Dylan Greene',
+    pubDate: 'May 20, 2012 04:00:00 GMT',
+    language: 'en',
+    ttl: '60',
+    itunesSubtitle: 'A show about everything',
+    itunesAuthor: 'John Doe',
+    itunesSummary: 'All About Everything is a show about everything. Each week we dive into any subject known to man and talk about it as much as we can. Look for our podcast in the Podcasts app or in the iTunes Store',
+    itunesOwner: {
+      name: 'John Doe',
+      email: 'john.doe@example.com',
+    },
+    itunesImage: 'http://example.com/podcasts/everything/AllAboutEverything.jpg',
+    itunesType: 'episodic',
+    itunesCategory: [{
+      text: 'Technology',
+      subcats: [{
+        text: 'Software',
+        subcats: [{
+          text: 'node.js',
+        }],
+      }],
+    }],
+  };
+  const item = {
+    title: 'item 1',
+    description: 'description 1',
+    url: 'http://example.com/article1',
+    date: 'May 24, 2012 04:00:00 GMT',
+    itunesAuthor: 'John Doe',
+    itunesSubtitle: 'A short primer on table spices',
+    itunesImage: 'http://example.com/podcasts/everything/AllAboutEverything/Episode1.jpg',
+    itunesDuration: '7:04',
+    itunesEpisode: 1,
+    itunesSeason: 1,
+    itunesTitle: 'itunes item 1',
+    itunesEpisodeType: 'full',
+  };
+  const feed = new Podcast(feedInfo, [item]);
+
+  t.is(feed.buildXml({ indent: '  ' }), expectedOutput.podcast.trim());
+})
 
 test('html content', (t) => {
   const feed = new Podcast({
