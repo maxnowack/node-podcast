b6bd87d5f5e88b88571f6223f2def8f1290c655a
Teun <teun@snappet.org>
2020-06-16 17:06:24 +0200

    Preserve backward compat for string-duration
    appeasing eslint
    
    eslint

--- src/durationFormat.js
+++ src/durationFormat.js
@@ -1,12 +1,15 @@
 function pad(num) {
-  const paddedString = ("0" + num);
-  return paddedString.substring(paddedString.length-2);
+  const paddedString = `0${num}`;
+  return paddedString.substring(paddedString.length - 2);
 }
 function toDurationString(seconds) {
-  const hh = Math.floor(seconds/(60*60));
-  seconds = seconds % (60*60);
-  const mm = Math.floor(seconds/60);
-  const ss = Math.floor(seconds % 60);
+  if (typeof seconds !== 'number') {
+    return seconds;
+  }
+  const hh = Math.floor(seconds / (60 * 60));
+  const remain = seconds % (60 * 60);
+  const mm = Math.floor(remain / 60);
+  const ss = Math.floor(remain % 60);
   return `${pad(hh)}:${pad(mm)}:${pad(ss)}`;
 }
 
